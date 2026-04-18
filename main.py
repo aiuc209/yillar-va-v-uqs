b2a3b4272d83c13f86879d7881450c3d082f2ebd
aiuc209 <dzuiryuomdbaoy@gmail.com>
2026-04-18 11:05:48 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,12 @@
+yillar_va_voqealar = [
+    ("1991", "Oʻzbekiston mustaqilligi eʼlon qilindi"),
+    ("1992", "Oʻzbekiston Respublikasining Birinchi Konstitutsiyasi qabul qilindi"),
+    ("1995", "Oʻzbekiston mustaqilligining 4-yilligi nishonlandi"),
+    ("2000", "Oʻzbekiston Prezidenti saylovi boʻlib oʻtdi"),
+    ("2010", "Oʻzbekiston mustaqilligining 19-yilligi nishonlandi"),
+    ("2015", "Oʻzbekiston Prezidenti saylovi boʻlib oʻtdi"),
+    ("2020", "Oʻzbekiston mustaqilligining 29-yilligi nishonlandi")
+]
+
+for yil, voqea in yillar_va_voqealar:
+    print(f"{yil} yilda: {voqea}")
